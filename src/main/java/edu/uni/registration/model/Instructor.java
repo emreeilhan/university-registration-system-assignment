688e040bcaac59d5721a161e1d1219ad958fc654
emreeilhan <emreilhn15@gmail.com>
2025-11-16 18:34:18 +0300

    Add Instructor model extending Person with department and officeNumber fields

--- src/main/java/edu/uni/registration/model/Instructor.java
+++ src/main/java/edu/uni/registration/model/Instructor.java
@@ -1,4 +1,38 @@
 package edu.uni.registration.model;
 
-public class Instructor {
+public class Instructor extends Person {
+    private String department;
+    private String officeNumber;
+
+    public Instructor(String id, String firstName, String lastName, String email,
+                      String department, String officeNumber) {
+        super(id, firstName, lastName, email);
+        this.department = department;
+        this.officeNumber = officeNumber;
+    }
+
+    // Getter methods
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    // Setter methods
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Department: " + department +
+                ", Office: " + officeNumber;
+    }
 }
