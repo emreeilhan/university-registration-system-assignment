3d5eeffda6777d0afaf3782f71510b481556cf7a
emreeilhan <emreilhn15@gmail.com>
2025-11-16 18:25:41 +0300

    Add Student model extending Person with major and year fields

--- src/main/java/edu/uni/registration/model/Student.java
+++ src/main/java/edu/uni/registration/model/Student.java
@@ -1,4 +1,37 @@
 package edu.uni.registration.model;
 
-public class Student {
+public class Student extends Person {
+    private String major;
+    private int year;
+
+    public Student(String id,String firstName,String lastName,String email,String major,int year) {
+        super(id,firstName,lastName,email);
+        this.major = major;
+        this.year = year;
+    }
+
+    //Getter methods
+    public String getMajor() {
+        return major;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    //Setter methods
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Major: " + major +
+                ", Year: " + year;
+    }
 }
