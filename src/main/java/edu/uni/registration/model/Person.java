eae2201e929c69bdfdc4ff9fc797792ce0674cfc
emreeilhan <emreilhn15@gmail.com>
2025-11-16 18:19:26 +0300

    Add Person abstract base class with fields, constructor, getters/setters, and toString()

--- src/main/java/edu/uni/registration/model/Person.java
+++ src/main/java/edu/uni/registration/model/Person.java
@@ -2,4 +2,59 @@ package edu.uni.registration.model;
 
 public abstract class Person {
 
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Person(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", Name: " + getFullName() +
+                ", Email: " + email;
+    }
 }
