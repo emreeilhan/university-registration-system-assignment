8c43f601514bd5027b6cc7e4797213ebe5d74da9
emreeilhan <emreilhn15@gmail.com>
2025-11-17 15:01:02 +0300

    Add generic Repository<T, ID> interface for basic in-memory CRUD operations

---
+++ src/main/java/edu/uni/registration/repository/Repository.java
@@ -0,0 +1,10 @@
+package edu.uni.registration.repository;
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T,ID> {
+    Optional<T> findById(ID id);
+    List<T> findAll();
+    T save(T entity);
+    void deleteById(ID id);
+}
