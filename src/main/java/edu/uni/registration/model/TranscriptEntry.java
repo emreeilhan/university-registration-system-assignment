37eb8ce9be952ef284512d800cbb25b72da6f4fa
emreeilhan <emreilhn15@gmail.com>
2025-11-17 14:29:44 +0300

    Add TranscriptEntry model to represent completed section grades with credits and quality points

--- src/main/java/edu/uni/registration/model/TranscriptEntry.java
+++ src/main/java/edu/uni/registration/model/TranscriptEntry.java
@@ -1,4 +1,46 @@
 package edu.uni.registration.model;
 
 public class TranscriptEntry {
+    private final Section section;
+    private final Grade grade;
+
+    public TranscriptEntry(Section section, Grade grade) {
+        if (section == null) {
+            throw new IllegalArgumentException("Section cannot be null");
+        }
+        if (grade == null) {
+            throw new IllegalArgumentException("Grade cannot be null");
+        }
+        this.section = section;
+        this.grade = grade;
+    }
+
+    //Getter methods
+    public Section getSection() {
+        return section;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public int getCredits(){
+        return section.getCourse().getCredits();
+    }
+
+    public double getQualityPoints(){
+        return getCredits() * grade.getPoints();
+    }
+
+    @Override
+    public String toString() {
+        return "TranscriptEntry{" +
+                "sectionId='" + section.getId() + '\'' +
+                ", courseCode='" + section.getCourse().getCode() + '\'' +
+                ", term='" + section.getTerm() + '\'' +
+                ", grade=" + grade +
+                ", credits=" + getCredits() +
+                ", qualityPoints=" + getQualityPoints() +
+                '}';
+    }
 }
