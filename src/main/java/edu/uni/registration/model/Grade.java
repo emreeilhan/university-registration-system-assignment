fa3df215f9a4fc0b04ba1ef2ce361e517d01d841
emreeilhan <emreilhn15@gmail.com>
2025-11-17 13:17:20 +0300

    Add Grade enum with GPA point values for transcript calculations

--- src/main/java/edu/uni/registration/model/Grade.java
+++ src/main/java/edu/uni/registration/model/Grade.java
@@ -1,5 +1,25 @@
 package edu.uni.registration.model;
 
 public enum Grade {
+    A(4.0),
+    B(3.0),
+    C(2.0),
+    D(1.0),
+    F(0.0);
+
+    private final double points;
+
+    Grade(double points) {
+        this.points = points;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    @Override
+    public String toString() {
+        return this.name();
+    }
 
 }
